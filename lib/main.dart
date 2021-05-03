23a69c1e174fcc4188a9f6d6f2c4d6e2570df689
xuqxu <froze666@me.com>
2021-05-03 19:54:37 +0900

    Firestoreに値を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'add/add_page.dart';
 import 'main_model.dart';
 
 void main() async {
@@ -14,32 +15,49 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TODOアプリ',
-      home: ChangeNotifierProvider<MainModel>(
-        create: (_) => MainModel()..getTodoListRealtime(),
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('TODOアプリ'),
-          ),
-          body: Consumer<MainModel>(builder: (context, model, child) {
-            final todoList = model.todoList;
-            return ListView(
-              children: todoList
-                  .map(
-                    (todo) => Card(
-                      child: ListTile(
-                        title: Text(todo.title),
-                      ),
-                    ),
-                  )
-                  .toList(),
-            );
-          }),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<MainModel>(
+      create: (_) => MainModel()..getTodoListRealtime(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('TODOアプリ'),
         ),
+        body: Consumer<MainModel>(builder: (context, model, child) {
+          final todoList = model.todoList;
+          return ListView(
+            children: todoList
+                .map(
+                  (todo) => Card(
+                    child: ListTile(
+                      title: Text(todo.title),
+                    ),
+                  ),
+                )
+                .toList(),
+          );
+        }),
+        floatingActionButton:
+            Consumer<MainModel>(builder: (context, model, child) {
+          return FloatingActionButton(
+            onPressed: () async {
+              await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddPage(model),
+                  fullscreenDialog: true,
+                ),
+              );
+            },
+            child: Icon(Icons.add),
+          );
+        }),
       ),
     );
   }
